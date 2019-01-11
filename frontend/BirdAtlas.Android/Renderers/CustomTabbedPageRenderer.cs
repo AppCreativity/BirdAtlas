ede2920d8abb4af57e6532017382bf2c2e8764e4
Depechie <glenn.versweyveld@gmail.com>
2019-01-11 10:55:57 +0100

    Added bottom tabbar android

--- frontend/BirdAtlas.Android/Renderers/CustomTabbedPageRenderer.cs
+++ frontend/BirdAtlas.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Android.Content;
 using Android.Support.Design.Widget;
 using Android.Views;
@@ -12,6 +14,34 @@ using Xamarin.Forms.Platform.Android.AppCompat;
 [assembly: ExportRenderer(typeof(CustomTabbedPage), typeof(CustomTabbedPageRenderer))]
 namespace BirdAtlas.Droid.Renderers
 {
+    public static class Extensions
+    {
+        public static List<Android.Views.View> GetViewsByType(this Android.Views.View view, Type viewType = null)
+        {
+            if (!(view is ViewGroup group))
+                return new List<Android.Views.View>();
+
+            var result = new List<Android.Views.View>();
+
+            for (int i = 0; i < group.ChildCount; i++)
+            {
+                var child = group.GetChildAt(i);
+                result.AddRange(child.GetViewsByType(viewType));
+
+                if (viewType == null || child.GetType() == viewType)
+                    result.Add(child);
+            }
+
+            return result.Distinct().ToList();
+        }
+
+        public static void FindAndRemoveById(this Android.Views.View view, int id)
+        {
+            var childView = view.FindViewById(id);
+            ((ViewGroup)childView.Parent).RemoveView(childView);
+        }
+    }
+
     public class CustomTabbedPageRenderer : TabbedPageRenderer
     {
         public CustomTabbedPageRenderer(Context context) : base(context)
@@ -22,19 +52,20 @@ namespace BirdAtlas.Droid.Renderers
         {
             base.OnElementPropertyChanged(sender, e);
 
-            if (e.PropertyName.Equals(CustomTabbedPage.IsHiddenProperty.PropertyName))
+            if (e.PropertyName.Equals(CustomTabbedPage.IsHiddenProperty.PropertyName) && Element is CustomTabbedPage customTabbed)
             {
-                TabLayout TabsLayout = null;
-                for (int i = 0; i < ChildCount; ++i)
+                var views = ViewGroup.GetViewsByType(typeof(BottomNavigationView));
+                if(views != null && views.Any())
                 {
-                    Android.Views.View view = (Android.Views.View)GetChildAt(i);
-                    if (view is TabLayout)
-                        TabsLayout = (TabLayout)view;
-                }
+                    var bottomView = views.FirstOrDefault() as BottomNavigationView;
+                    //bottomView.Visibility = customTabbed.IsHidden ? ViewStates.Invisible : ViewStates.Visible;
+                    //bottomView.TranslationY = customTabbed.IsHidden ? bottomView.TranslationY + bottomView.Height : bottomView.TranslationY - bottomView.Height;
 
-                if (Element is CustomTabbedPage customTabbed)
-                {
-                    TabsLayout.Visibility = customTabbed.IsHidden ? ViewStates.Invisible : ViewStates.Visible;
+                    //TODO: Glenn - Hmm hiding bottom tabbar leaves empty space in Android
+                    if(customTabbed.IsHidden)
+                        bottomView.Animate().TranslationY(bottomView.Height);
+                    else
+                        bottomView.Animate().TranslationY(0);
                 }
             }
         }
