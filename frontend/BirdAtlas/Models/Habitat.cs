f106ac69c85428622f53aa7ecc2e0941cdbabd3d
Depechie <glenn.versweyveld@gmail.com>
2020-04-15 22:37:03 +0200

    DiscoverTabView styling

---
+++ frontend/BirdAtlas/Models/Habitat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BirdAtlas.Models
+{
+    public class Habitat
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
--- frontend/BirdAtlas/ViewModels/DiscoverViewModel.cs
+++ frontend/BirdAtlas/ViewModels/DiscoverViewModel.cs
@@ -13,6 +13,13 @@ namespace BirdAtlas.ViewModels
             set => SetProperty(ref _stories, value);
         }
 
+        private ObservableCollection<Habitat> _habitats = new ObservableCollection<Habitat>();
+        public ObservableCollection<Habitat> Habitats
+        {
+            get => _habitats;
+            set => SetProperty(ref _habitats, value);
+        }
+
         private ObservableCollection<Nearby> _nearbyBirds = new ObservableCollection<Nearby>();
         public ObservableCollection<Nearby> NearbyBirds
         {
@@ -29,6 +36,10 @@ namespace BirdAtlas.ViewModels
             Stories.Add(new Story() { Category = "Science", Title = "Drunk birds? What is happening...", ImageUrl= "http://cdn.images.express.co.uk/img/dynamic/13/590x/549233_1.jpg" });
             Stories.Add(new Story() { Category = "The Wilds", Title = "Why we need to save the scavengers?", ImageUrl= "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/rufous_hummingbird_selasphorus_rufus_c_ryan_bushby_1.jpg" });
             Stories.Add(new Story() { Category = "Science", Title = "Drunk birds? What is happening...", ImageUrl= "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/shutterstock_459632779.jpg" });
+
+            Habitats.Add(new Habitat() { Name = "Grasslands", Amount = 290 });
+            Habitats.Add(new Habitat() { Name = "Tundra", Amount = 311 });
+            Habitats.Add(new Habitat() { Name = "Deserts", Amount = 307 });
 
             NearbyBirds.Add(new Nearby() { Name = "Eurasian hoopoe", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/1600/public/news/european_turtle_dove_streptopelia_turtur_websitec_revital_salomon.jpg" });
             NearbyBirds.Add(new Nearby() { Name = "Short-eared owl", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/little_spiderhunter_c_noicherrybeans_shutterstock_smaller_1.jpg" });
